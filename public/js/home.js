2f66527bae17b076bb9aeba669f74aa3ce1ead54
ano-27 <ramank27@outlook.com>
2025-05-16 11:06:01 +0530

    add logout feature

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('logoutElem').addEventListener('click', async () => {
-        try {
-            await fetch('/api/logout', {
-                method: 'POST',
-                credentials: 'include'    // Include cookies in the request. Better than sending refreshToken in body. Moreover, in Backend, we are handling currently via cookies only. We haven't set up to receive refreshToken in req.body
-            });
-            localStorage.removeItem('accessToken');
-            localStorage.removeItem('refreshToken');
-            window.location.href = '/login';
-        } catch (error) {
-            console.log(error);
-            localStorage.removeItem('accessToken');
-            localStorage.removeItem('accessToken');
-            window.location.href = '/login';
+        if (confirm('Proceed with Logout?')) {
+            try {
+                await fetch('/api/logout', {
+                    method: 'POST',
+                    credentials: 'include'    // Include cookies in the request. Better than sending refreshToken in body. Moreover, in Backend, we are handling currently via cookies only. We haven't set up to receive refreshToken in req.body
+                });
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('refreshToken');
+                window.location.href = '/login';
+            } catch (error) {
+                console.log(error);
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('accessToken');
+                window.location.href = '/login';
+            }
         }
     }); 
 });
